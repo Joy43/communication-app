d06b34fc991fd27b989a6f8512d52ece4529e3e6
ssjoy43@gmail.com <ssjoy43@gmail.com>
2026-01-19 00:56:47 +0600

    fix the issue now

---
+++ app/(product)/prouctpages.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const ProductPages = () => {
+  return (
+    <View>
+      <Text>Product Pages</Text>
+    </View>
+  )
+}
+
+export default ProductPages
